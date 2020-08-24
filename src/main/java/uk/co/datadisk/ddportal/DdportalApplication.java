e3a8d77e115f1d61e1aeb0738abb285d0a22da01
datadiskpfv <paul.valle@datadisk.co.uk>
2020-08-24 13:06:06 +0100

    Initial commit

---
+++ src/main/java/uk/co/datadisk/ddportal/DdportalApplication.java
@@ -0,0 +1,13 @@
+package uk.co.datadisk.ddportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdportalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DdportalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uk/co/datadisk/ddportal/DdportalApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.co.datadisk.ddportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdportalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
